8f1d361b1cafde4e191a1477bb085661f8779655
LenaMakarenko <l.makarenko96@mail.ru>
2023-02-06 18:41:39 +0300

    feat add task 1,2

--- hw2/index.ts
+++ hw2/index.ts
@@ -1,1 +1,19 @@
 console.log ('Hello, world!');
+//#### Task 1
+let x: number = 20;
+let y: number = 58;
+let z: number = 42;
+console.log (x+y+z);
+//  Task 2
+const numberOfSecIn1Min: number = 60;
+const numberOfMinIn1Hour: number = 60;
+const numberOfHourIn1Day: number = 24;
+const numberOfDayIn1Year: number = 365;
+let myAgeInSeconds: number = 26 * numberOfSecIn1Min * numberOfMinIn1Hour * numberOfHourIn1Day * numberOfDayIn1Year;
+//// Task 3
+let count = 42;
+let userName = '42';
+
+
+
+
