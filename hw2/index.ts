6727d531da27d1586fdb577d9987cea72d5d6c49
LenaMakarenko <l.makarenko96@mail.ru>
2023-02-06 23:51:38 +0300

    feat:+ write task 3,4

--- hw2/index.ts
+++ hw2/index.ts
@@ -13,6 +13,23 @@ let myAgeInSeconds: number = 26 * numberOfSecIn1Min * numberOfMinIn1Hour * numbe
 //// Task 3
 let count = 42;
 let userName = '42';
+let count_1 = String (count);
+let count_2 = count.toString ();
+console.log (typeof count_1);
+console.log (typeof count_2);
+let userName_1 = +userName;
+console.log (typeof userName_1);
+let userName_2 = Number (userName);
+console.log (typeof userName_2);
+///// Task 4
+let a = 1;
+let b = 2;
+let c = 'white bears';
+let sum = a + b + c;
+console.log (String (a) + b + ' ' + c);
+
+
+
 
 
 
