f2eb79ec356c98b4f529ef447853f423ba7dfbd3
Lena Makarenko <l.makarenko96@mail.ru>
2023-03-05 19:21:03 +0400

    feat add prettier

--- hw5/index.ts
+++ hw5/index.ts
@@ -1,39 +1,47 @@
 ///// Task 1
- {function getSum(x:number): void {
-    let result: number = x * (x + 1)/2;
-    console.log (result);
+{
+    function getSum(x: number): void {
+        let result: number = (x * (x + 1)) / 2
+        console.log(result)
     }
-    getSum(100);}
-    ///// Task 2
-    let interestRatePerYear: number = 0.17;
-    let numberOfYears: number = 5;
-    function overpaymentSum (x: number): void {
-    let result = x * interestRatePerYear * numberOfYears 
-    console.log (result)
-    }
-    overpaymentSum (100);
-    //// Task 3
-    function trimString (textString: string, valueFrom: number, valueBefore: number) {
-        console.log (textString.slice (valueFrom, valueBefore))
-    }
-    trimString ("TeachMeSkills", 3, 7)
-    //// Task 4
-    function getSumNumbers (x:number) {
-        let sum = 0;
-        let variet: string = String(x);
-        for (let i = 0; i < variet.length; i ++) {
-            sum += Number (variet [i]);
-        } console.log (sum)
-    }
-    getSumNumbers (2021)
-    /// Task 5
-    function getSum(a: number, b: number) {
-        console.log ((a  + b) * (Math.abs(a - b) + 1) / 2)
+    getSum(100)
 }
-    getSum (1, -2);
-    //// Task 6
-    const foo = () => console.log ("foo");
-    const boo = () => console.log ("boo");
-    function fooboo (z:boolean, foo: any, boo: any): any {
-    z ? foo() : boo()}
-    fooboo(true, foo, boo)
+///// Task 2
+let interestRatePerYear: number = 0.17
+let numberOfYears: number = 5
+function overpaymentSum(x: number): void {
+    let result = x * interestRatePerYear * numberOfYears
+    console.log(result)
+}
+overpaymentSum(100)
+//// Task 3
+function trimString(
+    textString: string,
+    valueFrom: number,
+    valueBefore: number
+) {
+    console.log(textString.slice(valueFrom, valueBefore))
+}
+trimString('TeachMeSkills', 3, 7)
+//// Task 4
+function getSumNumbers(x: number) {
+    let sum = 0
+    let variet: string = String(x)
+    for (let i = 0; i < variet.length; i++) {
+        sum += Number(variet[i])
+    }
+    console.log(sum)
+}
+getSumNumbers(2021)
+/// Task 5
+function getSum(a: number, b: number) {
+    console.log(((a + b) * (Math.abs(a - b) + 1)) / 2)
+}
+getSum(1, -2)
+//// Task 6
+const foo = () => console.log('foo')
+const boo = () => console.log('boo')
+function fooboo(z: boolean, foo: any, boo: any): any {
+    z ? foo() : boo()
+}
+fooboo(true, foo, boo)
--- hw6/index.ts
+++ hw6/index.ts
@@ -1,90 +1,87 @@
 ///// Task 1
 type NewYorker = {
-    name?: string | undefined;
+    name?: string | undefined
     age?: number | undefined
 }
-const newYorker1: NewYorker = { name: "Carrie Bradshow", 
-  age : 35,
-}
-console.log (newYorker1);
-delete newYorker1.age;
-delete newYorker1.name;
-console.log (newYorker1);
+const newYorker1: NewYorker = { name: 'Carrie Bradshow', age: 35 }
+console.log(newYorker1)
+delete newYorker1.age
+delete newYorker1.name
+console.log(newYorker1)
 /// Task 2
 type doctor = {
-speciality : string;
-"is highest category" : boolean
+    speciality: string
+    'is highest category': boolean
 }
 const doctor1: doctor = {
-  speciality : "ophtalmologist",
-  "is highest category" : true,
+    speciality: 'ophtalmologist',
+    'is highest category': true,
 }
-function searchForAKeySpeciality (object: any ): any {
-  if ("speciality" in object) console.log ('true')
-  else console.log (false)
+function searchForAKeySpeciality(object: any): any {
+    if ('speciality' in object) console.log('true')
+    else console.log(false)
 }
-searchForAKeySpeciality (doctor1)
-searchForAKeySpeciality (newYorker1)
+searchForAKeySpeciality(doctor1)
+searchForAKeySpeciality(newYorker1)
 //// Task 3
 type StudentType = {
-    name: string;
-    age: number;
-    isHappy: boolean;
+    name: string
+    age: number
+    isHappy: boolean
     [key: string]: string | number | boolean
 }
 const student: StudentType = {
-  name: 'John',
-  age: 19,
-  isHappy: true
+    name: 'John',
+    age: 19,
+    isHappy: true,
 }
 for (let key in student) {
-  console.log (key)
+    console.log(key)
 }
 for (let key in student) {
-  console.log (student[key])
+    console.log(student[key])
 }
 /// Task 4
 type ColorsType = {
     'ru pum pu ru rum': {
-        red: string;
-        green: string;
+        red: string
+        green: string
         blue: string
     }
 }
 const colors: ColorsType = {
-  'ru pum pu ru rum': {
-    red: 'reddish',
-    green: 'greenish',
-    blue: 'blueish'
-},
+    'ru pum pu ru rum': {
+        red: 'reddish',
+        green: 'greenish',
+        blue: 'blueish',
+    },
 }
-console.log( colors["ru pum pu ru rum"].red, 
-colors["ru pum pu ru rum"].blue)
-//// Task 5 
+console.log(colors['ru pum pu ru rum'].red, colors['ru pum pu ru rum'].blue)
+//// Task 5
 let salaries: any = {
     andrey: 500,
     sveta: 413,
     anton: 987,
     andrei: 664,
-    alexandra: 199
+    alexandra: 199,
 }
-let sumSalaries = 0;
+let sumSalaries = 0
 for (let key in salaries) {
-    sumSalaries += salaries[key] / 5;
+    sumSalaries += salaries[key] / 5
 }
-console.log (sumSalaries);
+console.log(sumSalaries)
 ////// Task 6
 let building = {
     foundation: 1997,
     country: 'USA',
     city: 'New Jersey',
-    generalInfo: function(): string {
-  return `This building was found in ${this.foundation} year in ${this.country}, ${this.city}.`
+    generalInfo: function (): string {
+        return `This building was found in ${this.foundation} year in ${this.country}, ${this.city}.`
     },
-    getDescriptionTextLength: function() : any {
-   let resultString: string = this.generalInfo();
-   return resultString.length
-    }
+    getDescriptionTextLength: function (): any {
+        let resultString: string = this.generalInfo()
+        return resultString.length
+    },
 }
-console.log (building.generalInfo());
-console.log (building.getDescriptionTextLength());
+console.log(building.generalInfo())
+console.log(building.getDescriptionTextLength())
