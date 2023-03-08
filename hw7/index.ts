b47b3c95de4642e5203235e1d2a971abaa52872b
Lena Makarenko <l.makarenko96@mail.ru>
2023-03-08 21:34:44 +0400

    feat add tasks 4-6

--- hw7/index.ts
+++ hw7/index.ts
@@ -12,3 +12,27 @@ console.log (numbers);
 const numbers2 = [5, 43, 63, 23, 90];
 numbers2.splice(0);
 console.log (numbers2);
+//// Task 4
+const students = ["Polina", "Dasha", "Masha"];
+students.pop();
+students.push("Borya");
+students.shift();
+students.unshift('Andrey');
+console.log (students);
+//// Task 5
+const cats = ["Gachito", "Tom", "Batman"];
+for (let i = 0; i < cats.length; i++) {
+    console.log (cats[i])
+}
+for (const cat of cats) {
+    console.log (cat)
+}
+//// Task 6
+const evenNumbers = [2, 4, 6, 8, 10];
+const oddNumbers = [1, 3, 5, 7, 9];
+const allNumbers = [...evenNumbers,...oddNumbers];
+console.log (allNumbers);
+console.log (allNumbers.indexOf(8));
+//// Task 7
+const binary = [0, 0, 0, 0];
+console.log (binary.join('1'));
