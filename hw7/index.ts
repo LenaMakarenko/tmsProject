4ea8b5390f3350e81b7bc45349327c9b5adc7e86
Lena Makarenko <l.makarenko96@mail.ru>
2023-03-08 21:32:48 +0400

    add hw7, tasks 1-3

---
+++ hw7/index.ts
@@ -0,0 +1,14 @@
+/// Task 1
+const colorsHw7: string [] = ["red", "green", "blue"];
+console.log (colorsHw7.length);
+//// Task 2
+const animals = ["monkey", "dog", "cat"];
+let lastNumberAr: number = animals.length - 1;
+console.log (animals[lastNumberAr])
+//// Task 3
+const numbers = [5, 43, 63, 23, 90];
+numbers.length = 0;
+console.log (numbers);
+const numbers2 = [5, 43, 63, 23, 90];
+numbers2.splice(0);
+console.log (numbers2);
