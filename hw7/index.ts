176f38543b9c24c0e31dd9d9eb78847c1bbfde5d
Lena Makarenko <l.makarenko96@mail.ru>
2023-03-11 00:25:34 +0400

    feat fix task 3

--- hw7/index.ts
+++ hw7/index.ts
@@ -1,5 +1,5 @@
 /// Task 1
-const colorsHw7: string [] = ["red", "green", "blue"];
+const colorsHw7 = ["red", "green", "blue"];
 console.log (colorsHw7.length);
 //// Task 2
 const animals = ["monkey", "dog", "cat"];
@@ -7,9 +7,9 @@ let lastNumberAr: number = animals.length - 1;
 console.log (animals[lastNumberAr])
 //// Task 3
 const numbers = [5, 43, 63, 23, 90];
+const numbers2 = [...numbers];
 numbers.length = 0;
 console.log (numbers);
-const numbers2 = [5, 43, 63, 23, 90];
 numbers2.splice(0);
 console.log (numbers2);
 //// Task 4
