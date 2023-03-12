6578517a6c30f0599d2609113eb4f0753f32881b
Lena Makarenko <l.makarenko96@mail.ru>
2023-03-12 13:20:35 +0400

    feat add tasks 5-9

--- hw8/index.ts
+++ hw8/index.ts
@@ -31,3 +31,42 @@ const sumOfFibonacci = fibonacci.reduce ((sum: number, item: number) => {
     return sum + item
 }, 0);
 console.log (sumOfFibonacci)
+//// Task 5
+const numbersOfTask5Hw8 = [5, 9, 13, 24, 54, 10, 13, 99, 1, 5]
+const searchOfFirstEvenNumber = numbersOfTask5Hw8.find((item: number) => {
+if (item % 2 === 0) return item
+})
+console.log (searchOfFirstEvenNumber)
+/// Task 6
+const isMultiple3And5 = [9, 67, 15, 30, 2].some((item: number) => {
+if (item % 3 === 0 && item % 5 === 0) return item
+}) 
+console.log (isMultiple3And5)
+//// Task 7
+const resultOfTask7: boolean = [2, 8].every((item: number) => {
+item**2
+let sum = 0
+let y = String(item)
+for ( let i = 0; i < y.length; i++){
+    sum += Number(y[i]);
+}
+if (sum % 2 === 0) return sum
+})
+console.log (resultOfTask7)
+//// Task 8
+const recipeMap = new Map<string, number>([
+    [`cucumber`, 300],
+    [`tomato`, 200],
+    [`salt`, 10],
+    [`sour cream`, 110]
+])
+recipeMap.forEach((item: number) => {
+    if (item > 100) {
+        console.log (item)
+    }
+})
+//// Task 9
+const numbersOfTask9 = [1, 6, 3, 2, 7, 325, 234, 54, 586, 23, 24, 64, 56,
+    3, 5, 7, 8, 1, 6, 8, 9, 4, 5, 61, 2, 4, 5, 6, 3, 5] 
+const setOfNumbers = new Set(numbersOfTask9);
+console.log(setOfNumbers)
