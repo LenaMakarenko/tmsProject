d0c039ed557ea65e57887c36de5c5e7c8ff1ee1f
LenaMakarenko <123626267+LenaMakarenko@users.noreply.github.com>
2023-03-21 20:24:09 +0300

    Hw9 (#8)
    * feat create hw9 branch, add md- files in all hw folders
    
    * feat add all tasks
    
    * feat fix task 1 and 2
    
    * feat fix access of properties

---
+++ hw9/index.ts
@@ -0,0 +1,31 @@
+/// Task 1
+class Animal {
+    public readonly name: string;
+    protected age: number;
+    static animalType: string = "cat";
+
+    constructor(animalName: string, animalAge: number) {
+        this.name = animalName;
+        this.age = animalAge;
+    }
+
+public getGreeting(): string {
+      return `This is a ${Animal.animalType}. It's name is ${this.name}`
+}
+}
+const animalOne = new Animal("Vasiliy", 3)
+console.log (animalOne.getGreeting())
+/// Task 2
+class Pet extends Animal {
+   private readonly address: string;
+   constructor (animalName: string, animalAge: number, petAddress: string) {
+    super (animalName, animalAge);
+    this.address = petAddress
+   }
+
+   public whereLives(): string {
+return `My ${Animal.animalType} lives in ${this.address}`
+   }
+}
+const petOne = new Pet("Barsik", 6, "Mogilev. Leninskaya street, 12" )
+console.log (petOne.whereLives())
