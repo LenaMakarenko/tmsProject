9e18afe2f81a10c5aab51b6eb5fc26fff484025e
Lena Makarenko <l.makarenko96@mail.ru>
2023-03-30 01:13:40 +0300

    feat fix task2

--- hw10/index.ts
+++ hw10/index.ts
@@ -1,42 +1,39 @@
 class Car {
-    constructor (
-        public carName: string, 
-        protected engineType: string ) {}
+    constructor(
+        public carName: string,
+        protected engineType: string) { }
 }
 class SportCar extends Car {
     constructor(
         public carName: string,
-        protected engineType: string, 
+        protected engineType: string,
         private maxSpeed: number,
-        public carCost: number ) {
+        public carCost: number) {
         super(carName, engineType)
-        this.maxSpeed = maxSpeed
-        this.carCost = carCost
     }
-        public getMaxSpeedAndCostOfTheCar() {
-            return `This is ${this.carName}. It has ${this.engineType} engine and max speed equal to ${this.maxSpeed} km/h. Approximetly cost of the car is ${this.carCost}$`
-        }
+    public getMaxSpeedAndCostOfTheCar() {
+        return `This is ${this.carName}. It has ${this.engineType} engine and max speed equal to ${this.maxSpeed} km/h. Approximetly cost of the car is ${this.carCost}$`
     }
+}
 class LuxaryCar extends Car {
     constructor(
         public carName: string,
-        protected engineType: string, 
-        private maxSpeed: number, 
-        public carCost: number ) {
+        protected engineType: string,
+        private maxSpeed: number,
+        public carCost: number) {
         super(carName, engineType)
-        this.maxSpeed = maxSpeed
-        this.carCost = carCost
     }
     public getMaxSpeedAndCostOfTheCar() {
         return `This is ${this.carName}. It has ${this.engineType} engine and max speed equal to ${this.maxSpeed} km/h. Approximetly cost of the car is ${this.carCost}$`
     }
 
 }
-const luxaryCar1  = new LuxaryCar ("Audi", "hgjhg", 120, 140000)
-console.log (luxaryCar1.getMaxSpeedAndCostOfTheCar())
-const sportCar1 = new SportCar ("Ford", "kjhg", 260, 2000000)
-console.log (sportCar1.getMaxSpeedAndCostOfTheCar())
-function getCarDesign(this: Car, numberOfDoors: number, steeringWheelSide: string) { 
-return `${this.carName} has ${numberOfDoors} doors and this is ${steeringWheelSide}-hand drive car.`}
-console.log (getCarDesign.call(sportCar1, 2, "left"))
-console.log (getCarDesign.call(luxaryCar1, 4, "right"))
+const luxaryCar1 = new LuxaryCar("Audi", "hgjhg", 120, 140000)
+console.log(luxaryCar1.getMaxSpeedAndCostOfTheCar())
+const sportCar1 = new SportCar("Ford", "kjhg", 260, 2000000)
+console.log(sportCar1.getMaxSpeedAndCostOfTheCar())
+function getCarDesign(numberOfDoors: number, steeringWheelSide: string): string {
+    return `${this.carName} has ${numberOfDoors} doors and this is ${steeringWheelSide}-hand drive car.`
+}
+console.log(getCarDesign.call(sportCar1, 2, "left"))
+console.log(getCarDesign.call(luxaryCar1, 4, "right"))
