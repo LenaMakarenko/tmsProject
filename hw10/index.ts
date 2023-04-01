47f5f3ea988c9e99f70bb85ce644ed4efdd86496
LenaMakarenko <123626267+LenaMakarenko@users.noreply.github.com>
2023-04-01 22:14:31 +0300

    Hw10 (#9)
    * add hw10 ts- file
    
    * feat add task1
    
    * feat add task 2
    
    * feat fix task2
    
    * feat fix task2
    
    * feat fix second task with a separate types of objects
    
    * feat fix text in function

---
+++ hw10/index.ts
@@ -0,0 +1,56 @@
+////Task 1
+class Car {
+    constructor(
+        public carName: string,
+        protected engineType: string) { }
+}
+class SportCar extends Car {
+    constructor(
+        public carName: string,
+        protected engineType: string,
+        private maxSpeed: number,
+        public carCost: number) {
+        super(carName, engineType)
+    }
+    public getMaxSpeedAndCostOfTheCar() {
+        return `This is ${this.carName}. It has ${this.engineType} engine and max speed equal to ${this.maxSpeed} km/h. Approximetly cost of the car is ${this.carCost}$`
+    }
+}
+class LuxaryCar extends Car {
+    constructor(
+        public carName: string,
+        protected engineType: string,
+        private maxSpeed: number,
+        public carCost: number) {
+        super(carName, engineType)
+    }
+    public getMaxSpeedAndCostOfTheCar() {
+        return `This is ${this.carName}. It has ${this.engineType} engine and max speed equal to ${this.maxSpeed} km/h. Approximetly cost of the car is ${this.carCost}$`
+    }
+
+}
+const luxaryCar1 = new LuxaryCar("Audi", "hgjhg", 120, 140000)
+console.log(luxaryCar1.getMaxSpeedAndCostOfTheCar())
+const sportCar1 = new SportCar("Ford", "kjhg", 260, 2000000)
+console.log(sportCar1.getMaxSpeedAndCostOfTheCar())
+//// Task 2
+type CarType = {
+    carName: string,
+    numberOfDoors: number,
+    steeringWheelSide: string
+}
+const firstCar: CarType = {
+    carName: "Skoda Rapid",
+    numberOfDoors: 4,
+    steeringWheelSide: "right"
+}
+const secondCar: CarType = {
+    carName: "Chevrolet g-45",
+    numberOfDoors: 3,
+    steeringWheelSide: "left"
+}
+function getCarDesign(): string {
+    return `This ${this.carName} has ${this.numberOfDoors} doors and this is ${this.steeringWheelSide}-hand drive car.`
+}
+console.log(getCarDesign.call(firstCar))
+console.log(getCarDesign.call(secondCar))
