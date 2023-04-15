deadf30d28d55c3c71cd186c183a8b8eaa1bedbf
Lena Makarenko <l.makarenko96@mail.ru>
2023-04-15 20:27:39 +0300

    feat add constans module

---
+++ hw12/constants/constans.ts
@@ -0,0 +1,24 @@
+import { Student } from "../types/types";
+const studentJohn: Student = {
+    name: "John",
+    surname: "Brown",
+    homeTown: "New Orlean",
+    age: 17
+}
+const studentSarah: Student = {
+    name: "Sarah",
+    surname: "Mayer",
+    homeTown: "Lisboa",
+    age: 21
+}
+const studentFrank: Student = {
+    name: "Frank",
+    surname: "Lampard",
+    homeTown: "London",
+    age: 18
+}
+export {
+    studentJohn,
+    studentSarah,
+    studentFrank
+}
---
+++ hw12/types/types.ts
@@ -0,0 +1,6 @@
+export type Student = {
+    name: string
+    surname: string
+    homeTown?: string
+    age: number
+}
