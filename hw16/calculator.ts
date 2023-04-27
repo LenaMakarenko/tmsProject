cc46edbb27620f32171d90440e4a9dc96b4d6db5
Lena Makarenko <l.makarenko96@mail.ru>
2023-04-27 17:42:33 +0300

    feat add positive & negative tests

--- hw16/calculator.ts
+++ hw16/calculator.ts
@@ -1,13 +1,13 @@
-export function myCalculator (firstArgument: number, secondArgument: number, operation: '+'| '-'| '*'|'/'): number {
+export function myCalculator(firstArgument: number, secondArgument: number, operation: '+' | '-' | '*' | '/'): number | string {
     switch (operation) {
-        case '+': 
-        return firstArgument + secondArgument;
-        case '-': 
-        return firstArgument - secondArgument;
-        case '*': 
-        return firstArgument * secondArgument; 
-        case '/': 
-        return firstArgument / secondArgument; 
+        case '+':
+            return firstArgument + secondArgument;
+        case '-':
+            return firstArgument - secondArgument;
+        case '*':
+            return firstArgument * secondArgument;
+        case '/':
+            if (!secondArgument) return "can't divide by zero"
+            return firstArgument / secondArgument;
     }
-    }
-    
+}
--- hw16/custom-checker.ts
+++ hw16/custom-checker.ts
@@ -1,7 +1,9 @@
 import { myCalculator } from "./calculator";
-export function myCalculatorChecker(firstArgument: number, secondArgument: number, operation: '+'| '-'| '*'|'/', resultChecker: number ) {
-    if ( myCalculator (firstArgument, secondArgument, operation) === resultChecker) {
-console.log ('Test passed!');}
-else { console.log ('Test failed!')
+export function myCalculatorChecker(firstArgument: number, secondArgument: number, operation: '+' | '-' | '*' | '/', resultChecker: number | string) {
+    if (myCalculator(firstArgument, secondArgument, operation) === resultChecker) {
+        console.log('Test passed!');
+    }
+    else {
+        console.log('Test failed!')
     }
 }
