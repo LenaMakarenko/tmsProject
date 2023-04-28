2ae92995a50a4934ed2473c94b3938bd407ee983
Lena Makarenko <l.makarenko96@mail.ru>
2023-04-28 12:36:32 +0300

    feat fix class calculator

--- hw17/classes/classes.ts
+++ hw17/classes/classes.ts
@@ -1,12 +1,8 @@
 export class Calculator {
-    constructor(){} 
-public getAdditionOperation(arg1: number, arg2: number)
-{return arg1 + arg2}
-public getSubstractionOperation(numb1: number, numb2: number)
-{return numb1 - numb2}
-public getMultiplicationOperation(param1: number, param2: number)
-{return param1 * param2}
-public getDivisionOperation(number1: number, number2: number){if(!number2) {return "Can't divide by zero!"} else number1/number2 }
+    constructor() { }
+    public getAdditionOperation(arg1: number, arg2: number) { return arg1 + arg2 }
+    public getSubstractionOperation(numb1: number, numb2: number) { return numb1 - numb2 }
+    public getMultiplicationOperation(param1: number, param2: number) { return param1 * param2 }
+    public getDivisionOperation(number1: number, number2: number) { if (!number2) { return "Can't divide by zero!" } else number1 / number2 }
 }
-const hgh = new Calculator
-console.log(hgh.getDivisionOperation(2,0))
+
