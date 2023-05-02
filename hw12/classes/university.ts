5085026bce1440cae9db47a4659dad978ff313a4
Lena Makarenko <l.makarenko96@mail.ru>
2023-05-02 20:08:31 +0300

    fix class University

--- hw12/classes/university.ts
+++ hw12/classes/university.ts
@@ -1,21 +1,19 @@
 const { Building } = require('./building')
 export class University extends Building {
-
-    public readonly name: string;
     public students: string[] = [];
     public studentsAge: number[] = [];
-    constructor(name: string) {
+    constructor(public readonly name: string) {
         super()
         this.name = name
     }
     public getStudentsList() {
-        if (this.students.length === 0) return `The studying year hasn't begun yet in ${this.name} university.`
-        else return this.students.toString()
+        if (this.students.length === 0) console.log (`The studying year hasn't begun yet in ${this.name} university.`)
+        else console.log(this.students.toString())
     }
 
     public getYoungestStudentAge() {
         const minStudentsAge = Math.min(...this.studentsAge)
-        if (this.studentsAge.length === 0) return "We are still waiting for new students."
-        else return `The youngest student we have in ${this.name} university is ${minStudentsAge} years old.`
+        if (this.studentsAge.length === 0) console.log ("We are still waiting for new students.")
+        else console.log (`The youngest student we have in ${this.name} university is ${minStudentsAge} years old.`)
     }
 }
