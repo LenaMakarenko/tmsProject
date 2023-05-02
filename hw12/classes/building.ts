4afc2c70ea35c29e60cb6c58a2538f82fb129570
Lena Makarenko <l.makarenko96@mail.ru>
2023-05-02 20:04:20 +0300

    fix building class

--- hw12/classes/building.ts
+++ hw12/classes/building.ts
@@ -1,9 +1,8 @@
 export class Building {
-    constructor (public foundationYear: number, public readonly city: string) {
-    this.foundationYear = foundationYear;
-    this.city = city}
+    constructor(public readonly foundationYear: number, public readonly city: string) {
+    }
 
     public getBuildingInfo() {
-        return `This building was found in ${this.foundationYear} in ${this.city}.`
+        console.log(`This building was found in ${this.foundationYear} in ${this.city}.`)
     }
 }
