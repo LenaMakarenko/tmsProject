d3d8a14a622276a6f087755da12778437aa132b2
Lena Makarenko <l.makarenko96@mail.ru>
2023-05-07 10:22:29 +0300

    install superagent

--- hw18/tests/reqres.spec.ts
+++ hw18/tests/reqres.spec.ts
@@ -1,1 +1,15 @@
 import {expect} from "@jest/globals";
+
+describe("Testing HTTP methods", () => {
+    test ("Should correctly read GET response",() => {
+        expect();
+    })
+
+    test ("Should ",() => {
+        expect();
+    })
+
+    test ("",() => {
+        expect();
+    })
+})
