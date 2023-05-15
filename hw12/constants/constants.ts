f18c998d6121f7a560d9f9426a4fb0db444ed375
LenaMakarenko <123626267+LenaMakarenko@users.noreply.github.com>
2023-05-15 19:54:28 +0300

    Hw12 (#11)
    * feat add building and university modules
    
    * feat add index modules
    
    * feat add constans module
    
    * feat add index.ts
    
    * feat fix function assignStudentToUniversity
    
    * feat fixed helpers.ts and building.ts
    
    * feat fixed try, catch
    
    * fix building class
    
    * fix class University
    
    * fix index.ts, constants.ts
    
    * fix class university
    
    * fix constructor university class
    
    * add default export for assignStudentToUniversity function

---
+++ hw12/constants/constants.ts
@@ -0,0 +1,24 @@
+import { Student } from "../types/types";
+const studentJohn: Student = {
+    name: "John",
+    surname: "Brown",
+    homeTown: "New Orlean",
+    age: 20
+};
+const studentSarah: Student = {
+    name: "Sarah",
+    surname: "Mayer",
+    homeTown: "Lisboa",
+    age: 14
+};
+const studentFrank: Student = {
+    name: "Frank",
+    surname: "Lampard",
+    homeTown: "London",
+    age: 45
+};
+export {
+    studentJohn,
+    studentSarah,
+    studentFrank
+}
---
+++ hw12/helpers/helpers.ts
@@ -0,0 +1,16 @@
+import { University } from "../classes/index"
+import { Student } from "../types/types"
+export default function assignStudentToUniversity(
+    university: University,
+    ...students: Student[]) {
+    students.forEach((student) => {
+        if (student.age >= 16) {
+            university.students.push(student.name + " " + student.surname);
+            if (!university.studentsAge.includes(student.age)) 
+                university.studentsAge.push(student.age)}
+            else {
+                throw new Error(`Error:We believe that ${student.name} ${student.surname} should finish school at first, as he/she is only ${student.age} years old`)
+            }
+        })
+    }
+
