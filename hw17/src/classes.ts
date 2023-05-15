243314fb9eb31da6f796c7a0b9bb5686a0683a43
Lena Makarenko <l.makarenko96@mail.ru>
2023-05-15 18:02:59 +0300

    rename methods of class Calculator

--- hw17/src/classes.ts
+++ hw17/src/classes.ts
@@ -1,7 +1,7 @@
 export class Calculator {
     constructor() { }
-    public getAdditionOperation(arg1: number, arg2: number) { return arg1 + arg2 }
-    public getSubtractionOperation(numb1: number, numb2: number) { return numb1 - numb2 }
-    public getMultiplicationOperation(param1: number, param2: number) { return param1 * param2 }
-    public getDivisionOperation(number1: number, number2: number) { if (number2===0)  return "Can't divide by zero!"; return number1 / number2
+    public getAdditionResult(arg1: number, arg2: number) { return arg1 + arg2 }
+    public getSubtractionResult(numb1: number, numb2: number) { return numb1 - numb2 }
+    public getMultiplicationResult(param1: number, param2: number) { return param1 * param2 }
+    public getDivisionResult(number1: number, number2: number) { if (number2===0)  return "Can't divide by zero!"; return number1 / number2
 }}
