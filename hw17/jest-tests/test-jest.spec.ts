c4a436822ef61484f991f460ba3e170f13a2370c
Lena Makarenko <l.makarenko96@mail.ru>
2023-05-15 18:51:16 +0300

    fix tests description

--- hw17/jest-tests/test-jest.spec.ts
+++ hw17/jest-tests/test-jest.spec.ts
@@ -4,48 +4,48 @@ const thisCalculator = new Calculator();
 
 describe("Testing the number addition method", () => {
     test ("Should correctly add two positive numbers",() => {
-        expect(thisCalculator.getAdditionOperation(100000000,20000)).toEqual(100020000);
+        expect(thisCalculator.getAdditionResult(100000000,20000)).toEqual(100020000);
     })
 
     test ("Should correctly add two negative numbers",() => {
-        expect(thisCalculator.getAdditionOperation(-26,-158)).toEqual(-184);
+        expect(thisCalculator.getAdditionResult(-26,-158)).toEqual(-184);
     })
 
     test ("Should correctly add negative and positive numbers",() => {
-        expect(thisCalculator.getAdditionOperation(-26,45)).toEqual(19);
+        expect(thisCalculator.getAdditionResult(-26,45)).toEqual(19);
     })
 })
 
 describe("Testing the number subtraction method", () => { 
     test ("Should correctly subtract two positive numbers",() => {
-        expect(thisCalculator.getSubtractionOperation(256,0)).toEqual(256);
+        expect(thisCalculator.getSubtractionResult(256,0)).toEqual(256);
     })
 
     test ("Should correctly subtract positive and negatve numbers",() => {
-        expect(thisCalculator.getSubtractionOperation(-25,19)).toEqual(-44);
+        expect(thisCalculator.getSubtractionResult(-25,19)).toEqual(-44);
     })
 })
 
 describe("Testing the number multiplication method", () => { 
     test ("Should correctly multiply two negative numbers",() => {
-        expect(thisCalculator.getMultiplicationOperation(256,0)).toEqual(0);
+        expect(thisCalculator.getMultiplicationResult(256,0)).toEqual(0);
     })
 
     test ("Should correctly multiply positive and negatve numbers",() => {
-        expect(thisCalculator.getMultiplicationOperation(-25,236)).toEqual(-5900);
+        expect(thisCalculator.getMultiplicationResult(-25,236)).toEqual(-5900);
     })
 })
 
 describe("Testing the number division method", () => { 
     test ("Should correctly divide two positive numbers",() => {
-        expect(thisCalculator.getDivisionOperation(256,0)).toEqual("Can't divide by zero!");
+        expect(thisCalculator.getDivisionResult(256,0)).toEqual("Can't divide by zero!");
     })
 
     test ("Should correctly divide positive and negatve numbers",() => {
-        expect(thisCalculator.getDivisionOperation(-24,4)).toEqual(-6);
+        expect(thisCalculator.getDivisionResult(-24,4)).toEqual(-6);
     })
 
     test ("Should correctly divide negative and positive numbers",() => {
-        expect(thisCalculator.getDivisionOperation(0,-150000000000.23)).toEqual(-0)
+        expect(thisCalculator.getDivisionResult(0,-150000000000.23)).toEqual(-0)
     })
 })
