c9069a4e52735a5fa20dd8efea019bd526e419e6
Lena Makarenko <l.makarenko96@mail.ru>
2023-06-06 13:55:46 +0300

    feat add tests to hw18

--- hw18/tests/jsonplaceholder.spec.ts
+++ hw18/tests/jsonplaceholder.spec.ts
@@ -1,73 +1,106 @@
 import { expect } from "@jest/globals";
 import superagent, { Response } from "superagent"
-
 let response: Response;
-const NUMBER_OF_POSTS = 100;
 const BASE_URL = `https://jsonplaceholder.typicode.com`;
+const NUMBER_OF_POSTS = 100;
+const EXAMPLE_OF_VALID_EMAIL = 'Eliseo@gardner.biz';
 const postNo = Math.floor(Math.random() * NUMBER_OF_POSTS) + 1;
-const NUMBER_OF_USERS = 10;
-const postCreated = {
-    title: 'Marilyn Manson',
-    body: 'Sweet dreems are made of this',
-    userId: Math.floor(Math.random() * NUMBER_OF_USERS)
-}
+const myPost: { name: string, age: number } = {
+    "name": "Lena",
+    "age": 27
+};
+const updatePost: { id: number, title: string, body: string } = {
+    "id": 15354,
+    "title": "hello, world!",
+    "body": "dure lex sed lex "
+};
 
 describe(`Testing HTTP methods on JSONplaceholder`, () => {
-    test(`Should correctly read GET response`, async () => {
+    test(`Should correctly read comments to posts`, async () => {
         try {
-            response = await superagent.get(`${BASE_URL}/posts`);
-        } catch (err: any) { throw new Error(err.message); }
-
+            response = await superagent.get(`${BASE_URL}/posts/${postNo}/comments`)
+        } catch (err: any) { throw new Error(err.message) }
         expect(response.status).toBe(200);
-        expect(response.body.length).toBe(NUMBER_OF_POSTS);
+        expect(response.body.length).toBe(5);
     })
 
-    test(`Should correctly GET post № ${postNo}`, async () => {
+    test(`Should correctly check GET response body (contains string)`, async () => {
         try {
-            response = await superagent.get(`${BASE_URL}/posts/${postNo}`);
-        } catch (err: any) { throw new Error(err.message); }
-
-        expect(response.status).toBe(200);
-        expect(response.body.id).toBe(postNo);
+            response = await superagent.get(`${BASE_URL}/posts/1/comments`)
+        } catch (err: any) { throw new Error(err.message) }
+        expect(response.text).toContain(EXAMPLE_OF_VALID_EMAIL)
     })
 
-    test(`Should correctly GET posts with definite postId ${postNo}`, async () => {
+    test(`Should correctly process non-existing post`, async () => {
         try {
-            response = await superagent
-                .get(`${BASE_URL}/posts`)
-                .query({ userId: postNo })
-        } catch (err: any) { throw new Error(err.message); }
-
-        expect(response.status).toBe(200);
-        response.body.forEach((post: any) => {
-            expect(post.userId).toBe(postNo)
-        })
-    })
-
-    test(`Should correctly handle non-existing post`, async () => {
-        try {
-            response = await superagent.get(`${BASE_URL}/posts/${NUMBER_OF_POSTS + 5}`);
+            response = await superagent.get(`${BASE_URL}/posts/${NUMBER_OF_POSTS + 5}`)
         } catch (err: any) {
             expect(err.status).toBe(404);
-        }
+        }})
+
+    test(`Should correctly DELETE post`, async () => {
+        try {
+            response = await superagent.delete(`${BASE_URL}/posts/${postNo}`);
+        } catch (err: any) { throw new Error(err.message) }
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({})
     })
 
-    test(`Should create a new post with POST method`, async () => {
+    test(`Should correctly DELETE unexisting post`, async () => {
+        try {
+            response = await superagent.delete(`${BASE_URL}/poss`);
+        } catch (err: any) {
+            expect(err.status).toBe(404)
+            expect(err.message).toBe("Not Found") }
+    })
+
+    test(`Should correctly do PUT method`, async () => {
+        try {
+            response = await superagent.put(`${BASE_URL}/posts/${postNo}`)
+                .set("Content-type", "application/json")
+                .send(myPost)
+        } catch (err: any) { throw new Error(err.message) }
+        expect(response.status).toBe(200);
+        expect(response.body).toMatchObject(myPost)
+    })
+
+    test(`Should correctly get response of PUT method`, async () => {
+        try {
+            response = await superagent.put(`${BASE_URL}/posts/${postNo}`)
+                .set("Content-type", "application/json")
+                .send(myPost)
+        } catch (err: any) { throw new Error(err.message) };
+        expect(response.status).toBe(200);
+        expect(response.text).toContain(myPost.name)
+    })
+
+    test(`Should correctly do POST method`, async () => {
         try {
             response = await superagent.post(`${BASE_URL}/posts`)
                 .set("Content-type", "application/json")
-                .send(postCreated);
-        } catch (err: any) {
-            throw new Error(err.message)}
-            expect(response.status).toBe(201)
-            expect(response.body).toMatchObject(postCreated)
+                .send(myPost)
+        } catch (err: any) { throw new Error(err.message) };
+        expect(response.status).toBe(201);
+        expect(response.body.id).toBe(NUMBER_OF_POSTS + 1)
     })
 
-    test(`Should testing HTTP PATCH method`, async () => {
+    test(`Should correctly process incorrect POST method`, async () => {
         try {
-            response = await superagent
-            .patch(`${BASE_URL}/posts/5`)
-            .set("Content-type", "application/json")
-            .send({ });
-
+            response = await superagent.post(`${BASE_URL}/propr`)
+                .set("Content-type", "application/json")
+                .send(myPost)
+        } catch (err: any) {
+            expect(err.status).toBe(404)
+        }
     })
+
+    test(`Should correctly do PATCH method`, async () => {
+        try {
+            response = await superagent.patch(`${BASE_URL}/posts/${postNo}`)
+                .set("Content-type", "application/json")
+                .send(updatePost)
+        } catch (err: any) { throw new Error(err.message) };
+        expect(response.status).toBe(200);
+        expect(response.body.title).toEqual(updatePost.title)
+    })
+})
