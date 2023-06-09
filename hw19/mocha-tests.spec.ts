1b02763039a53d56b7d92aeae14e04577d9c2260
Lena Makarenko <l.makarenko96@mail.ru>
2023-06-09 20:09:53 +0300

    feat install mocha

--- hw19/mocha-tests.spec.ts
+++ hw19/mocha-tests.spec.ts
@@ -1,17 +1,21 @@
 import { By, until, Builder, Capabilities } from "selenium-webdriver";
 
-const THIS_BASE_URL = "https://tabletka.by/";
+
+
+
+
+
+const THIS_BASE_URL = "https://quokkajs.com/";
 const driver = new Builder()
     .withCapabilities(Capabilities.chrome())
     .build();
 
-(async function testTabletkaBy() {
+(async function testQuokkajsSite() {
     try {
         await driver.manage().window().maximize;
         await driver.get(THIS_BASE_URL);
-        await driver.findElement(By.css("div[class='bttn bottom-notice-bttn allow-btn']")).click();
-        await driver.findElement(By.css("nav a[href='/bikes/']")).click();
-        await driver.wait(until.titleContains(" "), 2000)
+        await driver.findElement(By.css("nav a[href='/docs/#getting-started']")).click();
+        await driver.wait(until.titleContains("Quokka.js: Introduction"), 2000)
        
     } catch (err: any) { console.log(err.message) }
     finally { await driver.quit();}
