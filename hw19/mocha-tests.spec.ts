0d8e4705e8d4add41e0475cda5fcbddd31b55201
Lena Makarenko <l.makarenko96@mail.ru>
2023-06-12 15:29:40 +0300

    feat add 1 test

--- hw19/mocha-tests.spec.ts
+++ hw19/mocha-tests.spec.ts
@@ -1,15 +1,23 @@
 import { By, until, Builder, Capabilities } from "selenium-webdriver";
 import assert from "assert";
 import expect from "chai";
+
+
 const THIS_BASE_URL = "https://healthplanet.by";
 const driver = new Builder()
     .withCapabilities(Capabilities.chrome())
     .build();
 
-describe("UI tests on selenium for healthplanet.by", () => {
-it("should add item to the cart and then delete", () => {
-
-    })
+describe('UI tests on selenium for healthplanet', async () => {
+    before (async() => {
+await driver.manage().window().maximize();
+});
+it("Should go to 'the payment and shipping' section in the navigation menu", async () => {
+await driver.get(THIS_BASE_URL);
+await driver.findElement(By.css("a.main-nav__link[href='/oplata-i-dostavka/']")).click();
+await driver.wait(until.urlContains('/oplata-i-dostavka/'), 20000);
+await driver.quit();
+})
 })
 
 
@@ -17,13 +25,13 @@ it("should add item to the cart and then delete", () => {
 
 
 
-(async function testQuokkajsSite() {
-    try {
-        await driver.manage().window().maximize;
-        await driver.get(THIS_BASE_URL);
-        await driver.findElement(By.css("nav a[href='/docs/#getting-started']")).click();
-        await driver.wait(until.titleContains("Quokka.js: Introduction"), 2000)
+//(async function testQuokkajsSite() {
+ //   try {
+      //  await driver.manage().window().maximize;
+     //   await driver.get(THIS_BASE_URL);
+     //   await driver.findElement(By.css("nav a[href='/docs/#getting-started']")).click();
+     //   await driver.wait(until.titleContains("Quokka.js: Introduction"), 2000)
        
-    } catch (err: any) { console.log(err.message) }
-    finally { await driver.quit();}
-})();
+  //  } catch (err: any) { console.log(err.message) }
+ //   finally { await driver.quit();}
+//})();
