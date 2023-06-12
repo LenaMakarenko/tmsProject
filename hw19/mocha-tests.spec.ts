43534280a5281fea0ac43588f1c195b5a8bbbf88
Lena Makarenko <l.makarenko96@mail.ru>
2023-06-13 01:09:10 +0300

    feat fix first test

--- hw19/mocha-tests.spec.ts
+++ hw19/mocha-tests.spec.ts
@@ -2,6 +2,7 @@ import { By, until, Builder, Capabilities } from "selenium-webdriver";
 import {expect} from "chai";
 
 const THIS_BASE_URL = "https://healthplanet.by";
+const URL_FOR_FIRST_TEST = '/oplata-i-dostavka/';
 const driver = new Builder()
     .withCapabilities(Capabilities.chrome())
     .build();
@@ -15,13 +16,13 @@ await driver.manage().window().maximize();
 await driver.quit()
     });
 
-it("Should go to 'oplata i dostavka' section in the navigation menu", async () => {
+it("1 Should go to 'oplata i dostavka' section in the navigation menu", async () => {
 await driver.get(THIS_BASE_URL);
 await driver.findElement(By.css("a.main-nav__link[href='/oplata-i-dostavka/']")).click();
-await driver.wait(until.urlContains('/oplata-i-dostavka/'));
+await driver.wait(until.urlContains(URL_FOR_FIRST_TEST));
 }).timeout(100000);
 
-it("Should go to the product cart 'tabeks' from the catalog", async () => {
+it("2 Should go to the product cart 'tabeks' from the catalog", async () => {
 await driver.get(THIS_BASE_URL);
 await driver.findElement(By.css("div.catalog-menu__toggler span")).click();
 await driver.findElement(By.css("a.hover-menu__link[href^='/catalog/lekarstvennye-i-']")).click();
@@ -30,7 +31,7 @@ await driver.findElement(By.css("a.product-item__link[href^='/p/tabeks']")).clic
 await driver.wait(until.titleContains("Табекс таблетки"), 8000);
 }).timeout(30000)
 
-it("Should check search input field", async () => {
+it("3 Should check search input field", async () => {
     await driver.get(THIS_BASE_URL);
     await driver.findElement(By.css(".search__row  .search__input")).sendKeys("Ибандронат");
     await driver.findElement(By.css("a.search__btn span.hidden-xs")).click();
@@ -38,14 +39,14 @@ it("Should check search input field", async () => {
     expect(text).to.equal('Результаты по запросу «Ибандронат»');
     }).timeout(50000);
 
-it("Should check 'nashi apteki' button", async () => {
+it("4 Should check 'nashi apteki' button", async () => {
     await driver.get(THIS_BASE_URL);
     await driver.findElement(By.css("a.main-nav__link[href='/map/']")).click();
     const title = await driver.getTitle();
     expect(title).to.equal('География сети аптек "Планета Здоровья"')
 }).timeout(100000);
 
-it("Should check if the 'voyti'button is active", async () => {
+it("5 Should check if the 'voyti'button is active", async () => {
     await driver.get(THIS_BASE_URL);
     const checker = await driver.findElement(By.css("svg.symbol.symbol-tool-user")).isEnabled();
     expect(checker).to.equal(true)
