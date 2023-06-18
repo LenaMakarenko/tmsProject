bdc42218ccf90034e34c3b08b44e5754015a43f4
Lena Makarenko <l.makarenko96@mail.ru>
2023-06-19 00:06:07 +0300

    feat fix tests

--- hw19/mocha-tests.spec.ts
+++ hw19/mocha-tests.spec.ts
@@ -5,6 +5,7 @@ import { rmSync, mkdirSync, writeFileSync } from 'fs'
 const THIS_BASE_URL = "https://healthplanet.by";
 const URL_FOR_FIRST_TEST = '/oplata-i-dostavka/';
 const textToInput = "Ибандронат";
+const headerText = 'Результаты по запросу «Ибандронат»';
 const screenshotsDir = 'hw19/screenshots/';
 const driver = new Builder()
     .withCapabilities(Capabilities.chrome())
@@ -17,10 +18,6 @@ describe('UI tests on selenium for healthplanet', async () => {
         mkdirSync(screenshotsDir, { recursive: true });
         await driver.manage().setTimeouts({ implicit: 500000, pageLoad: 300000, script: 1000000 })
         await driver.manage().window().maximize();
-    });
-
-    beforeEach(async () => {
-        await driver.get(THIS_BASE_URL);
     });
     
     afterEach(async () => {
@@ -38,11 +35,13 @@ describe('UI tests on selenium for healthplanet', async () => {
     });
 
     it("1 Should go to 'oplata i dostavka' section in the navigation menu", async () => {
+        await driver.get(THIS_BASE_URL);
         await driver.findElement(By.css("a.main-nav__link[href='/oplata-i-dostavka/']")).click();
         await driver.wait(until.urlContains(URL_FOR_FIRST_TEST));
     }).timeout(100000);
 
     it("2 Should go to the product cart 'tabeks' from the catalog", async () => {
+        await driver.get(THIS_BASE_URL);
         await driver.findElement(By.css("div.catalog-menu__toggler span")).click();
         await driver.findElement(By.css("a.hover-menu__link[href^='/catalog/lekarstvennye-i-']")).click();
         await driver.findElement(By.css("a.filter__categories-link[href^='/catalog/borba-s-vrednymi']")).click();
@@ -51,19 +50,22 @@ describe('UI tests on selenium for healthplanet', async () => {
     }).timeout(30000)
 
     it("3 Should check search input field", async () => {
+        await driver.get(THIS_BASE_URL);
         await driver.findElement(By.css(".search__row  .search__input")).sendKeys(textToInput);
         await driver.findElement(By.css("a.search__btn span.hidden-xs")).click();
         const text = await driver.findElement(By.xpath("//h1[@class='page-title h2']")).getText();
-        expect(text).to.equal('Результаты по запросу Ибандронат');
+        expect(text).to.equal(headerText);
     }).timeout(50000);
 
     it("4 Should check 'nashi apteki' button", async () => {
+        await driver.get(THIS_BASE_URL);
         await driver.findElement(By.css("a.main-nav__link[href='/map/']")).click();
         const title = await driver.getTitle();
         expect(title).to.equal('География сети аптек "Планета Здоровья"')
     }).timeout(100000);
 
     it("5 Should check if the 'voyti'button is active", async () => {
+        await driver.get(THIS_BASE_URL);
         const checker = await driver.findElement(By.css("svg.symbol.symbol-tool-user")).isEnabled();
         expect(checker).to.be.true
     }).timeout(100000);
