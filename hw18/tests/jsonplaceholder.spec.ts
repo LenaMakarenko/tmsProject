53368815708272899e5e922867b769fd6f665051
Lena Makarenko <l.makarenko96@mail.ru>
2023-06-18 20:21:08 +0300

    feat fix tests

--- hw18/tests/jsonplaceholder.spec.ts
+++ hw18/tests/jsonplaceholder.spec.ts
@@ -6,6 +6,7 @@ import {validateSchema} from "D:/Project/tmsProject/hw18/helpers";
 let response: Response;
 const BASE_URL = `https://jsonplaceholder.typicode.com`;
 const NUMBER_OF_POSTS = 100;
+const NUMBER_OF_COMMENTS_TO_ONE_POST = 5;
 const postNo = Math.floor(Math.random() * NUMBER_OF_POSTS) + 1;
 const myPost: { name: string, age: number } = {
     "name": "Lena",
@@ -23,7 +24,7 @@ describe(`Testing HTTP methods on JSONplaceholder`, () => {
             response = await superagent.get(`${BASE_URL}/posts/${postNo}/comments`)
         } catch (err: any) { throw new Error(err.message) }
         expect(response.status).toBe(200);
-        expect(response.body.length).toBe(5);
+        expect(response.body.length).toBe(NUMBER_OF_COMMENTS_TO_ONE_POST);
     })
 
     test(`Should correctly check GET response body`, async () => {
@@ -35,7 +36,7 @@ describe(`Testing HTTP methods on JSONplaceholder`, () => {
 
     test(`Should correctly process non-existing post`, async () => {
         try {
-            response = await superagent.get(`${BASE_URL}/posts/${NUMBER_OF_POSTS + 5}`)
+            response = await superagent.get(`${BASE_URL}/posts/${NUMBER_OF_POSTS + 1}`)
         } catch (err: any) {
             expect(err.status).toBe(404);
         }})
@@ -63,17 +64,16 @@ describe(`Testing HTTP methods on JSONplaceholder`, () => {
                 .send(myPost)
         } catch (err: any) { throw new Error(err.message) }
         expect(response.status).toBe(200);
-        expect(response.body).toMatchObject(myPost)
+        expect(response.body).toMatchObject(myPost);
+        expect(response.text).not.toContain("title");
     })
 
-    test(`Should correctly get response of PUT method`, async () => {
+    test(`Should correctly process incorrect PUT method`, async () => {
         try {
-            response = await superagent.put(`${BASE_URL}/posts/${postNo}`)
+            response = await superagent.put (`${BASE_URL}/poss/${postNo} + 1`)
                 .set("Content-type", "application/json")
                 .send(myPost)
-        } catch (err: any) { throw new Error(err.message) };
-        expect(response.status).toBe(200);
-        expect(response.text).toContain(myPost.name)
+        } catch (err: any) { expect(err.message).toBe("Not Found")};
     })
 
     test(`Should correctly do POST method`, async () => {
@@ -83,7 +83,8 @@ describe(`Testing HTTP methods on JSONplaceholder`, () => {
                 .send(myPost)
         } catch (err: any) { throw new Error(err.message) };
         expect(response.status).toBe(201);
-        expect(response.body.id).toBe(NUMBER_OF_POSTS + 1)
+        expect(response.body.id).toBe(NUMBER_OF_POSTS + 1);
+        expect(response.body).toMatchObject(myPost)
     })
 
     test(`Should correctly process incorrect POST method`, async () => {
@@ -103,6 +104,8 @@ describe(`Testing HTTP methods on JSONplaceholder`, () => {
                 .send(updatePost)
         } catch (err: any) { throw new Error(err.message) };
         expect(response.status).toBe(200);
-        expect(response.body.title).toEqual(updatePost.title)
+        expect(response.body.title).toEqual(updatePost.title);
+        expect(response.body.id).toEqual(updatePost.id);
+        expect(response.body.body).toEqual(updatePost.body);
     })
 })
