d0219e58f8b682ed778c2013ab2776209a2a4f7b
Lena Makarenko <l.makarenko96@mail.ru>
2023-08-09 16:10:27 +0300

    feat add intercept

--- hw24/cypress/e2e/healtplanet-basket.cy.ts
+++ hw24/cypress/e2e/healtplanet-basket.cy.ts
@@ -1,17 +1,24 @@
 import { PageFactory } from "../support/pages/pageFactory";
 import { PAGES } from "../support/types/types";
-import { BasketPage} from "../support/pages/basket_page";
+import { basketPage, BasketPage} from "../support/pages/basket_page";
 import { HomePage } from "../support/pages/home_page";
 
 describe('HealthPlanet Site Tests - BasketPage', () => {
+   before(() => {
+     cy.intercept("POST", "https://healthplanet.by/bitrix/tools/conversion/ajax_counter.php").as("tracker")
+    })
+    
     it(`Should check the empty basket`, () => {
         const basketPage = PageFactory.getPage(PAGES.BASKET) as BasketPage
         const homePage = PageFactory.getPage(PAGES.HOME) as HomePage
         cy.viewport(1280, 720)
         homePage.visitPage();
         homePage.navigationBar.clickOnBasket();
+        cy.wait('@tracker').then(data => {
+        expect(data.response?.statusCode).to.equal(200);
+        expect(data.response?.body.length).to.equal(2);
+        
         basketPage.getTitleOfEmptyBasket().should('have.text', " В корзине\n нет товаров\n ")
-    });
-
-
+    })
+})
 })
